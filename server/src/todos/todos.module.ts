8b6e2217c17e3c14c9c28fc8dcbc99ee6a6bafac
Adam Sohn <adamsohn@Adams-MacBook-Pro.local>
2022-02-24 13:00:33 -0400

    Removed previous instance of nestjs

--- server/src/todos/todos.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TodosResolver } from './todos.resolver';
-import { TodosService } from './todos.service';
-
-@Module({
-  providers: [TodosResolver, TodosService],
-})
-export class TodosModule {}
